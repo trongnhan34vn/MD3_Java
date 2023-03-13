094282d5035a63b403ddb70e856aa0df426d6fb8
Mac <trongnhan34vn@gmail.com>
2023-03-13 08:56:57 +0700

    update

--- Session 6/src/Bai4_ShapeTriangle/Main.java
+++ Session 6/src/Bai4_ShapeTriangle/Main.java
@@ -5,14 +5,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Nhập vào 3 cạnh tam giác");
-        System.out.println("a: ");
-        double a = sc.nextDouble();
-        System.out.println("b: ");
-        double b = sc.nextDouble();
-        System.out.println("c: ");
-        double c = sc.nextDouble();
-        Triangle triangle = new Triangle("red",false, a,b,c);
-        System.out.println("Tam giác của bạn là: " + triangle);
+        while (true) {
+            System.out.println("Nhập vào 3 cạnh tam giác");
+            System.out.println("a: ");
+            double a = sc.nextDouble();
+            System.out.println("b: ");
+            double b = sc.nextDouble();
+            System.out.println("c: ");
+            double c = sc.nextDouble();
+            if(a+b > c && a+c > b && c+b > a) {
+                Triangle triangle = new Triangle("red",false, a,b,c);
+                System.out.println("Tam giác của bạn là: " + triangle);
+                return;
+            } else {
+                System.out.println("Nhập lại! Tổng 2 cạnh tam giác phải lớn hơn cạnh còn lại");
+            }
+        }
     }
 }
